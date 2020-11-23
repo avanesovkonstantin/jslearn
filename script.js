fe1bb84ff46501dbd13bce47030df1210043ea86
Konstantin <tihon.aka.kot@gmail.com>
2020-11-23 22:45:17 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+"use sctrict";
+
+//let res = confirm("are you here?");
+res = [];
+res[0] = prompt('Как тебя зовут?', "");
+res[1] = prompt('Как твоя фамилия?', "");
+res[2] = prompt('Сколько тебе лет?', "");
+
+//console.log(res + 5);
+document.write(res);
