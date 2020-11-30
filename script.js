256a40957f824273ba1ecf25f809b46996ddafee
Konstantin <tihon.aka.kot@gmail.com>
2020-11-30 23:10:13 +0300

    пытаемся еще раз

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-"use sctrict";
+let numberOfFilm = +prompt("how many movies?", "");
+let personalMovieDb = {
+    count: numberOfFilm,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let i = 0;
+while (i < 2) {
+    let ans1 = prompt("the last one?", "");
+    let ans2 = prompt("rate it2?", "");
+    personalMovieDb.movies[ans1] = ans2;
+    i++;
+}
+
+console.log(personalMovieDb);
