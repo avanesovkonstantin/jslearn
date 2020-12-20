ab73f3150ba563211d13753fb446b3918bc40f27
Konstantin <tihon.aka.kot@gmail.com>
2020-12-20 16:51:55 +0300

    урок про функции практика

---
+++ script functions.js
@@ -0,0 +1,12 @@
+"use strict";
+
+"use strict";
+
+let num = 12.2;
+
+console.log(Math.round(num));
+
+let test = "12.2px";
+
+console.log(parseInt(test));
+console.log(parseFloat(test));
--- script.js
+++ script.js
@@ -1,18 +1,68 @@
-let numberOfFilm = +prompt("how many movies?", "");
+"use strict";
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов ты посмотрел уже,", "");
+    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов ты посмотрел уже,", "");
+    }
+}
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        let latest = getAnswerControlLenght("what was the latest film do you see?");
+        let rating = getAnswerControlLenght("rat it");
+        personalMovieDb.movies[latest] = rating;
+    }
+    console.log(personalMovieDb);
+
+}
+
+function detectPersonalLevel() {
+    if (personalMovieDb.count < 10) {
+        alert("its too little");
+    } else if (personalMovieDb.count < 30) {
+        alert("you are tipical");
+    } else {
+        alert("you are fan of films!");
+    }
+}
+
+function getAnswerControlLenght(question) {
+    let answer = '';
+    answer = prompt(question, "");
+    if (answer == null || answer.length == 0 || answer.length > 2) {
+        getAnswerControlLenght(question);
+    }
+
+    return answer;
+}
+
+function showMyDB() {
+    if (personalMovieDb.privat == true) {
+        console.log(personalMovieDb);
+    }
+
+}
+
+function writeYourGenres() {
+    let arr = [];
+    for (let i = 0; i < 3; i++) {
+        arr.push(prompt(`Ваш любимый жанр под номером ${i+1}`, ""));
+    }
+
+    return arr;
+}
+
 let personalMovieDb = {
-    count: numberOfFilm,
+    count: numberOfFilms,
     movies: {},
     actors: {},
-    genres: [],
+    genres: {},
     privat: false
 };
 
-let i = 0;
-while (i < 2) {
-    let ans1 = prompt("the last one?", "");
-    let ans2 = prompt("rate it2?", "");
-    personalMovieDb.movies[ans1] = ans2;
-    i++;
-}
 
+personalMovieDb.genres = writeYourGenres();
 console.log(personalMovieDb);
